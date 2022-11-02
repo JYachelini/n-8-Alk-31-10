7e20144f76f5ddd2131b9317d3cdd57d8ad4f9d4
Danilo Cortes <dacortes63@misena.edu.co>
2022-11-02 18:10:13 -0500

    chore<web server>:create model and migration of transaction
    create model and migration of transaction without relations between tables

---
+++ database/migrations/20221102213152-create-transaction.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      amount: {
+        type: Sequelize.DECIMAL,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+      },
+      date: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Transactions');
+  },
+};
---
+++ database/models/transaction.js
@@ -0,0 +1,31 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate() {
+      // define association here
+    }
+  }
+  Transaction.init(
+    {
+      description: DataTypes.STRING,
+      amount: DataTypes.DECIMAL,
+      userId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+      date: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      timestamps: true,
+      paranoid: true,
+      deletedAt: 'destroyTime',
+      modelName: 'Transaction',
+    }
+  );
+  return Transaction;
+};
