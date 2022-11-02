433e7459510cb1eda67b194799ceee31fb937c67
CynGames <tomasm.leguizamon@gmail.com>
2022-11-02 13:35:01 -0300

    Removed id

--- database/models/category.js
+++ database/models/category.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
     /**
@@ -9,19 +7,21 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-    }
-  };
-  Category.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    deletedAt: {
-      type: DataTypes.STRING
+    static associate() {}
+  }
+  Category.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      deletedAt: {
+        type: DataTypes.STRING,
+      },
     },
-  }, {
-    sequelize,
-    timestamps: true,
-    modelName: 'Category',
-  });
+    {
+      sequelize,
+      timestamps: true,
+      modelName: 'Category',
+    }
+  );
   return Category;
 };
