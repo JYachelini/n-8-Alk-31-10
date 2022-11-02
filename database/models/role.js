79a29dec976ba363f1b3af99c68d941d7416fa58
CynGames <tomasm.leguizamon@gmail.com>
2022-11-01 21:11:11 -0300

    <feat> <config> N8A31-8 Crear modelo y migracion de Role

--- database/models/role.js
+++ database/models/role.js
@@ -1,28 +1,44 @@
+/*
+    Campos:
+    id: INTEGER NOT NULL AUTO_INCREMENT
+    name: STRING NOT NULL
+    description: STRING NULLABLE
+    timestamps
+*/
+
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Role extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      Role.hasMany(models.User, { foreignKey: 'roleId' });
-    }
-  };
-  Role.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    deletedAt: {
-      type: DataTypes.STRING
-    },
-  }, {
-    sequelize,
-    timestamps: true,
-    modelName: 'Role',
-  });
-  return Role;
+    class Role extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            Role.hasMany(models.User, {foreignKey: 'roleId'});
+        }
+    };
+    Role.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    }, {
+        sequelize,
+        timestamps: true,
+        modelName: 'Role',
+    });
+    return Role;
 };
