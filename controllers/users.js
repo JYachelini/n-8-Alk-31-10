c1afc3b0a174f831070b6338211cf683fd9e7ae1
AndresDP <andresdallapozza@gmail.com>
2022-11-02 16:13:50 -0300

    <web server> (26) endpoint-update-user

--- controllers/users.js
+++ controllers/users.js
@@ -44,4 +44,29 @@ module.exports = {
       next(httpError);
     }
   }),
+
+  update: catchAsync(async (req, res, next) => {
+    try {
+      const data = req.body;
+      const response = await User.update(data, {
+        where: { id: req.params.id },
+      });
+      console.log(response);
+      if (!response[0] == 0) {
+        endpointResponse({
+          res,
+          message: "Users updated successfully",
+          body: response,
+        });
+      } else {
+        res.status(400).json({ message: "id not found or nathing to change " });
+      }
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error updating users] - [index - GET]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { get, remove } = require("../controllers/users");
+const { get, remove, update } = require("../controllers/users");
 
 const router = express.Router();
 
 router.get("/", get);
 router.delete("/:id", remove);
+router.put("/:id", update);
 
 module.exports = router;
