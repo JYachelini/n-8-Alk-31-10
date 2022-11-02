9097d875fee9f241e7363b11ff48cf4d7724f7ed
Andres Dalla Pozza <84631947+AndresDP25@users.noreply.github.com>
2022-11-02 20:41:31 -0300

    N8 a31 28 endpoint update users (#9)
    * <web-server> (26) endpoint delete user
    
    * <web server> (26) endpoint-update-user
    
    * mod
    
    * <web-server>(28)endpoint update user fixes
    
    * <web-server>(28)endpoint update user fixes
    
    Co-authored-by: Julián Yachelini <juliyachedev@gmail.com>

--- database/models/role.js
+++ database/models/role.js
@@ -1,7 +1,6 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-
   class Role extends Model {
     /**
      * Helper method for defining associations.
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const {
-   get,
-} = require('../controllers/users')
+const express = require("express");
+const { get, remove, update } = require("../controllers/users");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', get)
+router.get("/", get);
+router.delete("/:id", remove);
+router.put("/:id", update);
 
-
-module.exports = router
+module.exports = router;
