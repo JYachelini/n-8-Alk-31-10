1047a16d14d717e0f8b939a9e45b9dca50aff438
AndresDP <andresdallapozza@gmail.com>
2022-11-02 19:19:42 -0300

    N8A31-26-endpoint-delete-users - fixes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { get, remove } = require("../controllers/users");
+const { check, param } = require("express-validator");
 
 const router = express.Router();
 
-router.get("/", get);
-router.delete("/:id", remove);
+const Validator = [param("id").isInt().withMessage("id must be an integer")];
 
+router.get("/", get);
+router.delete("/:id", Validator, remove);
 module.exports = router;
