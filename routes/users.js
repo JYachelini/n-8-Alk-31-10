bb635bc57773fb37af070a596812bfb3d30d0321
CynGames <tomasm.leguizamon@gmail.com>
2022-11-03 20:58:43 -0300

    <feat> Endpoint para datos de Usuario

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const { get, remove, update, create } = require('../controllers/users');
+const {
+  get,
+  remove,
+  update,
+  create,
+  getById,
+} = require('../controllers/users');
 const { userCreate } = require('../schemas/userCreateSchema');
 const { validator } = require('../middlewares/validator');
 
 const router = express.Router();
 
 router.get('/', get);
+router.get('/:id', getById);
 router.post('/', validator(userCreate), create);
 router.delete('/:id', remove);
 router.put('/:id', update);
