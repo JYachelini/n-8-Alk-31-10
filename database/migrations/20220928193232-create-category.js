63687ee6779802a30ba82dac4a4b8ca99502c815
José Luis Rangel <82683440+josrenyer@users.noreply.github.com>
2022-11-03 11:00:34 -0500

    <feat> (init) crear modelo y migracion category
    * <feat> (init) crear modelo y migracion category
    
    * <feat>(init) Crear Modelo y Migracion Category

--- database/migrations/20220928193232-create-category.js
+++ database/migrations/20220928193232-create-category.js
@@ -6,25 +6,31 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     await queryInterface.dropTable('Categories');
-  }
+  },
 };
