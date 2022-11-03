3b1ac7eb3140901468e82cdb12e1c033f4dd3bc2
EmiBaglioni <emi.baglioni@outlook.com>
2022-11-03 18:47:45 -0300

    chore <web server>: create routing and controller to list transactions

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const usersRouter = require('./users');
+const transactionsRouter = require('./transactions');
 const categoriesRouter = require('./categories');
 
 const router = express.Router();
 
 // example of a route with index controller get function
 router.use('/users', usersRouter);
+router.use('./transactions', transactionsRouter);
 router.use('/categories', categoriesRouter);
 
 module.exports = router;
