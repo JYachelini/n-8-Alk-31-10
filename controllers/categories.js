c9a27d733dd192052d3a90741eb67202c272066a
CynGames <tomasm.leguizamon@gmail.com>
2022-11-04 16:18:57 -0300

    <feat> Endpoint para crear categorias

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,6 +2,7 @@ const createHttpError = require('http-errors');
 const { Category } = require('../database/models');
 const { endpointResponse } = require('../helpers/success');
 const { catchAsync } = require('../helpers/catchAsync');
+const { ErrorObject } = require('../helpers/error');
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
@@ -21,4 +22,68 @@ module.exports = {
       next(httpError);
     }
   }),
+
+  create: catchAsync(async (req, res, next) => {
+    try {
+      const { name, description } = req.body;
+      const [response, created] = await Category.findOrCreate({
+        where: {
+          name,
+        },
+        defaults: {
+          name,
+          description,
+        },
+      });
+
+      if (!created) throw new ErrorObject('Category might already exist', 409);
+
+      endpointResponse({
+        res,
+        message: 'Category created successfully',
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error creating the category] - [POST]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
 };
+
+/*
+create: catchAsync(async (req, res, next) => {
+    try {
+      let { firstName, lastName, email, password } = req.body;
+      password = await bcrypt.hashData(password, 10);
+      const [response, created] = await User.findOrCreate({
+        where: {
+          email,
+        },
+        defaults: {
+          firstName,
+          lastName,
+          email,
+          password,
+        },
+      });
+
+      if (!created)
+        return res.status(400).json({ message: 'user or email already exist' });
+
+      endpointResponse({
+        res,
+        message: 'success',
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving users] - [index - GET]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+ */
