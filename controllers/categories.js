2d5e5b96737db65ed716007fef734b1fbc38301d
EmiBaglioni <emi.baglioni@outlook.com>
2022-11-05 13:17:13 -0300

    chore <web server>: create routing and controller to show categories by id

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,6 +1,7 @@
 const { Category } = require('../database/models');
 const { endpointResponse } = require('../helpers/success');
 const { catchAsync } = require('../helpers/catchAsync');
+const { ErrorObject } = require('../helpers/error');
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
@@ -16,7 +17,21 @@ module.exports = {
       next(error);
     }
   }),
+  getById: catchAsync(async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const response = await Category.findByPk(id);
 
+      if (!response) throw new ErrorObject('Category not found', 404);
+      endpointResponse({
+        res,
+        message: 'Category retrieved successfully',
+        body: response,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }),
   create: catchAsync(async (req, res, next) => {
     try {
       const { name, description } = req.body;
--- routes/categories.js
+++ routes/categories.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { validator } = require('../middlewares/validator');
 const { categoryCreate } = require('../schemas/categoryCreateSchema');
 const { create } = require('../controllers/categories');
-const { get } = require('../controllers/categories');
+const { get, getById } = require('../controllers/categories');
 
 const router = express.Router();
 
 router.get('/', get);
+router.get('/:id', getById);
 router.post('/', validator(categoryCreate), create);
 
 module.exports = router;
