7ba3ad7e689403011aeb58f06cc92ccb40366407
Emiliana A. Baglioni <103010477+EmilianaABaglioni@users.noreply.github.com>
2022-11-05 13:07:39 -0300

    chore <web server>: create routing and controller to list transactions (#28)

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { get, create } = require('../controllers/transactions');
+const { get, getById, create } = require('../controllers/transactions');
 const { validator } = require('../middlewares/validator');
 const { transactionsCreate } = require('../schemas/transactionsCreateSchema');
 
 const router = express.Router();
 
 router.get('/', get);
+router.get('/:id', getById);
 router.post('/', validator(transactionsCreate), create);
 
 module.exports = router;
