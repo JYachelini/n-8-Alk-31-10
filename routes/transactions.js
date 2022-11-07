734403b641f2277b850f304eb3bd39a4cefec1fc
AndresDP <andresdallapozza@gmail.com>
2022-11-07 09:23:58 -0300

    <feat>Endpoint-update-transactions

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { 
-get, 
-getById, 
-create,
-remove
+const {
+  get,
+  getById,
+  create,
+  update,
+  remove,
 } = require('../controllers/transactions');
 const { validator } = require('../middlewares/validator');
 const { transactionsCreate } = require('../schemas/transactionsCreateSchema');
@@ -13,7 +14,7 @@ const router = express.Router();
 router.get('/', get);
 router.delete('/:id', remove);
 router.get('/:id', getById);
+router.put('/:id', validator(transactionsCreate), update);
 router.post('/', validator(transactionsCreate), create);
-
 
 module.exports = router;
