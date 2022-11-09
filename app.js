d0637607a5e5ca8be8c665b42ebf1a00afbaf38f
jeronimo olivera <omegablak17@gmail.com>
2022-11-08 23:20:37 -0300

    'f'

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 
+app.use('/uploads',express.static(path.join(__dirname, 'tmp')))
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
