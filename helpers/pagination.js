55801d644a10a71bf6f5e57e0b34718b9c24fa05
jeronimo olivera <omegablak17@gmail.com>
2022-11-09 13:23:37 -0300

    <refactor><web-server>pagination-users-and-update-paginate

--- helpers/pagination.js
+++ helpers/pagination.js
@@ -1,28 +1,21 @@
 require('dotenv').config();
-const HOST = process.env.HOST
+const HOST = process.env.HOST;
 const PORT = process.env.PORT || 3000;
 
-const paginationUrls = async (Model,page) => {
-    
-    const route = `${Model.name}s`.toLocaleLowerCase()
-    const size = 10
-    const count = await Model.count();
-        return {
-            next: 
-            count-10 > (page*size)
-            ? `${HOST}:${PORT}/${route}?page=${Number(page)+1}` 
-            : null,
-            prev:
-            page > 0
-            ? `${HOST}:${PORT}/${route}?page=${Number(page)-1}` 
-            : null  
-        }
+const paginationUrls = async (Model, page) => {
+  const route = `${Model.name}s`.toLocaleLowerCase();
+  const size = 10;
+  const count = await Model.count();
+  return {
+    count: count,
+    next:
+      count - 10 > page * size
+        ? `${HOST}:${PORT}/${route}?page=${Number(page) + 1}`
+        : null,
+    prev: page > 0 ? `${HOST}:${PORT}/${route}?page=${Number(page) - 1}` : null,
+  };
+};
 
-    
-
-
-}
-
-module.exports ={
-    paginationUrls
+module.exports = {
+  paginationUrls,
 };
