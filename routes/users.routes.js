4a382403a5680db44d3b6f6f09ee6ce56c19ca23
Jeronimo Olivera <86572008+oliverajdev@users.noreply.github.com>
2022-11-09 18:44:05 -0300

    <feat><middleware>add-upload-images
    * f
    
    * <feat><middleware>add-upload-images

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,12 +3,17 @@ const express = require('express');
 const { userController } = require('../controllers');
 
 const { userSchema, paramSchema } = require('../schemas');
-const { validator, ownership } = require('../middlewares');
+const { validator, ownership, upload } = require('../middlewares');
 
 const router = express.Router();
 
 router.get('/', userController.get);
-router.post('/', validator(userSchema.create), userController.create);
+router.post(
+  '/',
+  upload.single('avatar'),
+  validator(userSchema.create),
+  userController.create
+);
 router.get(
   '/:id',
   validator(paramSchema.validatorId),
