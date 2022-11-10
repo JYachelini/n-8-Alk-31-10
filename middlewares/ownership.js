0cfaa04ca8d1635a60e1aeda0228cfaf10af758a
jeronimo olivera <omegablak17@gmail.com>
2022-11-09 23:28:41 -0300

    <fixed><web-server>create-user

--- middlewares/ownership.js
+++ middlewares/ownership.js
@@ -4,18 +4,23 @@ const { ErrorObject } = require('../helpers/error');
 const ownership = async (req, res, next) => {
   try {
     const { id } = req.params;
-
     const { query } = req.query;
+
+    if (!req.headers.authorization) {
+      throw new ErrorObject('You must log in', 403);
+    }
 
     let token = req.headers.authorization.split(' ').pop();
 
     let tokenDecode = decode(token);
 
-    if (tokenDecode.roleId == 1) return next();
-
-    if (tokenDecode.id == query) return next();
-
-    if (tokenDecode.id == id) return next();
+    if (
+      tokenDecode.roleId == 1 ||
+      tokenDecode.id == query ||
+      tokenDecode.id == id
+    ) {
+      return next();
+    }
 
     throw new ErrorObject('not allowed', 403);
   } catch (error) {
--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -13,7 +13,7 @@ const checkAuth = async (req, res, next) => {
     }
     next();
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
 
