a87968dbcffea69eeb18b93be936d877a5bb7c17
Julián Yachelini <juliyachedev@gmail.com>
2022-11-10 12:08:12 -0300

    <feat> (Auth) Register (#53)
    * a
    
    * <feat> (Auth) Register

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
-const { authController } = require('../controllers');
-const { authSchema } = require('../schemas');
-const { validator } = require('../middlewares');
+const { authController, userController } = require('../controllers');
+const { authSchema, userSchema } = require('../schemas');
+const { validator, upload } = require('../middlewares');
 
 const router = Router();
 
 router.post('/login', validator(authSchema.login), authController.login);
+router.post(
+  '/register',
+  validator(userSchema.create),
+  upload.single('avatar'),
+  userController.create
+);
 
 module.exports = router;
