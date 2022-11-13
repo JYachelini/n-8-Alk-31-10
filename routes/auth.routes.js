9b7644ad5e1e7b453b7cc7b152bc84bf93d30630
MartinFiguerola <fedefiguerola@gmail.com>
2022-11-13 18:28:30 -0300

    fix-crear modelo-y-migración-de-user

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,53 +4,61 @@ const { authSchema, userSchema } = require('../schemas');
 const { validator, upload } = require('../middlewares');
 
 const router = Router();
-/**
- * @swagger
- *  definitions:
- *   loginUser:
- *    type: object
- *    properties:
- *      email:
- *        type: string
- *        example: admin@admin.com
- *      password:
- *        type: string
- *        example: 12345678
- *   tokenUser:
- *      type: object
- *      properties:
- *        body:
- *         type: string
-
- */
-
+// --- LOGIN ---
 /**
  * @swagger
  * /auth/login:
  *  post:
- *     tags:
- *       - Auth
- *     summary: login
- *     description: login user
- *     requestBody:
- *         description: Created user object
+ *    tags:
+ *      - Auth
+ *    summary: Log in
+ *    description: Login user into the system
+ *    requestBody:
+ *      description: user object
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - email
+ *              - password
+ *            properties:
+ *              email:
+ *                type: string
+ *                default: admin@admin.com
+ *              password:
+ *                type: string
+ *                default: 12345678
+ *    responses:
+ *      200:
+ *          description: OK
+ *          content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: true
+ *                 code:
+ *                   type: integer
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: Some message of success
+ *                 body:
+ *                  type: object
+ *                  example: Some token
+ *      4XX:
+ *         description: FAILED
  *         content:
- *          application/json:
- *           schema:
- *             $ref: '#/definitions/loginUser'
- *     responses:
- *       '200':
- *         description: Users retrieved successfully
- *         content:
- *          application/json:
- *           schema:
- *             $ref: '#/definitions/tokenUser'
- *       '404':
- *         description: Error searching users
- *         content:
- *          application/json:
- *           schema:
- *             $ref: '#/definitions/error'
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Some error message
  */
 
 router.post('/login', validator(authSchema.login), authController.login);
@@ -70,18 +78,35 @@ router.post('/login', validator(authSchema.login), authController.login);
  *           schema:
  *             $ref: '#/definitions/userSchema'
  *     responses:
- *       '200':
- *         description: Users retrieved successfully
+ *      200:
+ *          description: OK
+ *          content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: true
+ *                 code:
+ *                   type: integer
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: Some message of success
+ *                 body:
+ *                  type: object
+ *                  example: Some token
+ *      4XX:
+ *         description: FAILED
  *         content:
  *           application/json:
  *             schema:
- *                 $ref: '#/definitions/userSchema'
- *       '404':
- *         description: Error searching users
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/definitions/error'
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Some error message
  */
 
 router.post(
